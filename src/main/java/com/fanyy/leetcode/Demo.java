1b9c9c5c449ba87574ad5ee86dde9a0ff5378e1e
fanyuanyuan <fanyuanyuan@qiyi.com>
2021-12-14 21:43:22 +0800

    update 86/2449
    Signed-off-by: fanyuanyuan <fanyuanyuan@qiyi.com>

--- src/main/java/com/fanyy/leetcode/Demo.java
+++ src/main/java/com/fanyy/leetcode/Demo.java
@@ -9,29 +9,10 @@ import java.util.*;
 
 public class Demo {
     public static void main(String[] args) {
-        int[] nums = {1, 3, 6, 8, 33, 0, 9, 27,66, 88};
-        int p1 = 0;
-        int p2 = 0;
-        int p3 = 0;
-        int[] a = {0, 9, 27, 66, 88};
-        while(p1 < 5 && p2 < 5) {
-            if (nums[p1] <= a[p2]) {
-                nums[p3++] = nums[p1++];
-            } else {
-                nums[p3++] = a[p2++];
-            }
-        }
-
-        while(p1 < 5) {
-            nums[p3++] = nums[p1++];
-        }
-
-        while(p2 < 5) {
-            nums[p3++] = a[p2++];
-        }
-
-        for(int i: nums ) {
-            System.out.println(nums[i] + " ");
+        int x = 4;
+        while(x > 0) {
+            int rx = x & 1;
+            x >>= 1;
         }
 
     }
